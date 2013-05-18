beeedbdfff39da3e0366c79ef1f76b4ffa0ecafc
Xaiter <Xaiter@gmail.com>
2013-05-18 10:22:26 -0700

    Added missing namespaces.

--- DistributedTaskProcessing/DataUtilities.cs
+++ DistributedTaskProcessing/DataUtilities.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
