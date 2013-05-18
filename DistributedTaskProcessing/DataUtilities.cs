ac5c9cd0ce86389fd7cd6eeaaefd6be6937beea0
Xaiter <Xaiter@gmail.com>
2013-05-18 10:19:25 -0700

    Initial Commit
    Here we go, Jet.

---
+++ DistributedTaskProcessing/DataUtilities.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedTaskProcessing
+{
+    /// <summary>
+    /// Provides common binary data manipulation functionality.
+    /// </summary>
+    public static class DataUtilities
+    {
+        // Public Methods
+        public static byte[] Serialize(object value)
+        {
+            var formatter = new BinaryFormatter();
+            var ms = new MemoryStream();
+
+            formatter.Serialize(ms, value);
+            return ms.ToArray();
+        }
+
+        public static T Deserialize<T>(byte[] data)
+        {
+            var formatter = new BinaryFormatter();
+            var ms = new MemoryStream(data);
+            return (T)formatter.Deserialize(ms);
+        }
+
+        public static byte[] Compress(byte[] bytes)
+        {
+            byte[] retValue;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (GZipStream zipStream = new GZipStream(stream, CompressionMode.Compress, true))
+                {
+                    zipStream.Write(bytes, 0, bytes.Length);
+                    retValue = stream.ToArray();
+                    zipStream.Close();
+                    stream.Close();
+                }
+            }
+            return retValue;
+        }
+
+        public static byte[] Decompress(byte[] bytes)
+        {
+            byte[] retValue;
+            using (MemoryStream stream = new MemoryStream())
+            {
+                using (GZipStream zipStream = new GZipStream(new MemoryStream(bytes), CompressionMode.Decompress, true))
+                {
+                    byte[] buffer = new byte[4096];
+                    int size;
+                    while (true)
+                    {
+                        size = zipStream.Read(buffer, 0, buffer.Length);
+                        if (size > 0)
+                            stream.Write(buffer, 0, size);
+                        else
+                            break;
+                    }
+                    zipStream.Close();
+                    retValue = stream.ToArray();
+                    stream.Dispose();
+                    zipStream.Dispose();
+                    return retValue;
+                }
+            }
+        }
+
+        public static AssemblyName[] GetDependentAssemblies(Assembly root)
+        {
+            throw new NotImplementedException();
+        }
+        
+        
+
+
+        // Private Methods
+        private static void GetDependentAssembliesRecursive(Assembly currentRoot, Dictionary<string, AssemblyName> dependencies)
+        {
+            foreach (var reference in currentRoot.GetReferencedAssemblies())
+            {
+
+            }
+        }
+
+
+        // Extension Methods
+        public static Assembly GetAssembly(this AssemblyName name)
+        {
+            return Assembly.Load(name);
+        }
+    }
+}
---
+++ DistributedTaskProcessing/ITaskProgram.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DistributedTaskProcessing
+{
+    /// <summary>
+    /// A program to be loaded by a TaskServer (probably through a user interface).
+    /// </summary>
+    public interface ITaskProgram
+    {
+        string Name { get; set; }
+        Assembly[] GetAssemblies();
+        WorkItemMessage[] GetWorkItemMessages();
+    }
+
+    /// <summary>
+    /// Accepts work from ITaskPrograms and distributes it to ITaskClients.
+    /// </summary>
+    public class TaskServer : ITaskServer
+    {
+        public List<ClientInformation> Clients { get; set; }
+
+        public void DoWork(ITaskProgram program)
+        {
+            var workItems = new Queue<WorkItemMessage>(program.GetWorkItemMessages());
+
+
+        }
+
+        public void RegisterClient(string endpointUrl)
+        {
+            var clientInfo = new ClientInformation();
+            clientInfo.EndpointLocation = endpointUrl;
+            clientInfo.LastMessage = DateTime.Now;
+
+        }
+
+        public void ReceiveWorkItemUpdate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Providers functionality to add reigster workers
+    /// and receive work progress updates.
+    /// </summary>
+    public interface ITaskServer
+    {
+        void RegisterClient(string endpointUrl);
+        void ReceiveWorkItemUpdate();
+    }
+
+    /// <summary>
+    /// Provides functionality for a client to receive work from a server.    
+    /// </summary>
+    public interface ITaskClient
+    {
+        bool HasProgram();
+        void ReceiveProgram(Stream message);
+        void ExecuteWorkItem(Stream message);
+        bool IsAlive();
+    }
+
+    public class ClientInformation
+    {
+        // Properties
+        public string EndpointLocation { get; set; }
+        public DateTime LastMessage { get; set; }
+        public bool IsAlive { get; set; }
+    }
+
+    public class WorkItemMessage
+    {
+        public Guid WorkItemId { get; set; }
+        public byte[] WorkData { get; set; }
+        public string WorkDataType { get; set; }
+
+        public string ProgramName { get; set; }
+        public string WorkerType { get; set; }
+        public string WorkerMethodName { get; set; }
+    }
+
+    public class ProgramMessage
+    {
+        public string Name { get; set; }
+        public FileData[] Assemblies { get; set; }
+    }
+
+    public class FileData
+    {
+        public string Filename { get; set; }
+        public byte[] Data { get; set; }
+    }
+
+
+}
